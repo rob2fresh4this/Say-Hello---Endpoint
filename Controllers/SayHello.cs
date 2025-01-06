bac94e5fb68703530e9a57b78665461f56ad0f50
rob2fresh4this <grijrob07@gmail.com>
2025-01-06 15:22:01 -0800

    first commit

---
+++ Controllers/SayHello.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;// this allows us to add our api controller
+
+namespace SayHello.Controllers
+{
+    [ApiController]// this tells the file to read the api controller
+    [Route("[controller]")]// removing the api/ we nolonger need to type the api to the url
+    public class SayHelloController : ControllerBase
+    {
+        public List<string> sayHello = new();
+
+        public SayHelloController(){
+            
+        }
+
+
+        [HttpPost]
+        [Route("Addname/{nameToAdd}")]
+        public List<string> Addname(string nameToAdd)
+        {
+            sayHello.Add("Hello " + nameToAdd);
+            return sayHello;
+        }     
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Say Hello - Endpoint
+
+## Overview
+Design an API endpoint that receives user input and outputs a personalized greeting, such as "Hello, {user's name}."
+
+## Instructions
+1. **Design** an API endpoint that accepts user input (such as a name).
+2. **Return** a personalized greeting message, like "Hello, {user's name}."
+3. **Utilize** Postman to facilitate the data retrieval process.
+
+## Deliverables
+- A zipped project containing the necessary files.
+- A GitHub link in the comments section of your code.
+
+## Note
+- This project **WILL NOT** be hosted on Vercel.
