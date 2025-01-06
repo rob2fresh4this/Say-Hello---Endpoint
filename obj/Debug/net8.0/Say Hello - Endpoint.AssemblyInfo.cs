85f5193307e431a904ab618ecd2244219cbf24df
rob2fresh4this <grijrob07@gmail.com>
2025-01-06 15:32:49 -0800

    1

--- obj/Debug/net8.0/Say Hello - Endpoint.AssemblyInfo.cs
+++ obj/Debug/net8.0/Say Hello - Endpoint.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Say Hello - Endpoint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bac94e5fb68703530e9a57b78665461f56ad0f50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c77116779507b865d1c89e50c6eb1b9ca4d6d931")]
 [assembly: System.Reflection.AssemblyProductAttribute("Say Hello - Endpoint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Say Hello - Endpoint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
